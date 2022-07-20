863235aabbb4e946b83986c2e566a789f8c0e5e7
Amadeusz Polak <amadeusz.polak@gmail.com>
2022-07-20 10:49:17 +0200

    Print amount of products bought

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ for shop in shop_dict:
     temp_list = [each_string.capitalize() for each_string in shop_dict[shop]]
     print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {temp_list}")
     product_counter += len(shop_dict[shop])
+print(f"W sumie kupuje {product_counter} produktów")
