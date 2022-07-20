11b0040faf5970bbce72db93dabdd225cd8a3780
Amadeusz Polak <amadeusz.polak@gmail.com>
2022-07-20 10:47:08 +0200

    Add iteration through shopping dict to print shops and products

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 shop_dict = {'piekarnia': ['chleb', 'pączek', 'bułki'], 'warzywniak': ['marchew', 'seler', 'rukola']}
 product_counter = 0
+for shop in shop_dict:
+    temp_list = [each_string.capitalize() for each_string in shop_dict[shop]]
+    print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {temp_list}")
+    product_counter += len(shop_dict[shop])
